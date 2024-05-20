00a492aef99848198e222ad5fc79f49f682f40db
Adrian <adrianearl16@gmail.com>
2024-05-21 04:43:13 +0800

    Add project files.

---
+++ CarBrandClassifier/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CarBrandClassifier
+{
+    public partial class Form1 : Form
+    {
+        private MLLoader MLLoader;
+
+        public Form1()
+        {
+            InitializeComponent();
+            MLLoader = new MLLoader(@"C:\Users\admin\source\repos\CarBrandClassifier\CarBrandClassifier\MLModel1.zip");
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.ShowDialog();
+            if (ofd.FileName != null)
+            {
+                pictureBox1.ImageLocation = ofd.FileName;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image != null)
+            {
+                // Convert the image to bytes
+                ImageConverter converter = new ImageConverter();
+                byte[] imageBytes = (byte[])converter.ConvertTo(pictureBox1.Image, typeof(byte[]));
+
+                // Prepare input data for prediction
+                ModelInput input = new ModelInput
+                {
+                    ImageSource = imageBytes
+                };
+
+                // Make prediction
+                var prediction = MLLoader.Predict(input);
+
+                // Display the predicted breed
+                MessageBox.Show("Selected file is an image of a " + prediction.Prediction);
+            }
+            else
+            {
+                MessageBox.Show("Please select an image first.");
+            }
+        }
+    }
+}
---
+++ CarBrandClassifier/MLLoader.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CarBrandClassifier;
+using Microsoft.ML;
+
+
+namespace CarBrandClassifier
+{
+    public class MLLoader
+    {
+        private string modelFilePath;
+        private MLContext mlContext;
+        private ITransformer mlModel;
+
+        public MLLoader(string modelFilePath)
+        {
+            this.modelFilePath = modelFilePath;
+            mlContext = new MLContext();
+
+
+            using (var stream = new System.IO.FileStream(modelFilePath, System.IO.FileMode.Open))
+            {
+                mlModel = mlContext.Model.Load(stream, out var schema);
+            }
+        }
+
+        public ModelOutput Predict(ModelInput input)
+        {
+
+            var predictionEngine = mlContext.Model.CreatePredictionEngine<ModelInput, ModelOutput>(mlModel);
+            var result = predictionEngine.Predict(input);
+            return result;
+        }
+    }
+}
